c774d8f8c8db03fa3c09cf45012cfc3a67a33910
Analía Felix <annegomez4456@gmail.com>
2023-09-27 19:22:33 -0300

    first commit

---
+++ sript.js
@@ -0,0 +1,29 @@
+function enviarData () {
+    let nombre = document.getElementById('nombre').value;
+let apellido = document.getElementById('apellido').value;
+let fecha = document.getElementById('date').value;
+let datos = {
+    nombre:nombre,
+    apellido:apellido,
+    fecha: fecha
+}
+let url = 'https://jsonplaceholder.typicode.com/users'
+ fetch (url, {
+    method: 'POST',
+    body: JSON.stringify(datos),
+    headers:{
+        'Content-Type':'application/json',
+    },
+ })
+ .then((res)=> res.json())
+ .catch((error)=>console.error('error',error))
+ .then((response)=>console.log('Datos enviados',response));
+
+ document.getElementById('nombre').value = '';
+ document.getElementById('apellido').value = '';
+ document.getElementById('date').value = '';
+
+}
+
+let btn = document.getElementById('send')
+btn.addEventListener('click', enviarData);
